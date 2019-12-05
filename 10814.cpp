a564f997f4b8ff8d067e7fd7c99bd537ea8522bb
leeshinyook <tlsdbr97@gmail.com>
2019-12-06 00:03:10 +0900

    나이순 정렬 #정렬(vector-pair-stable_sort)

---
+++ 10814.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+bool cmp(pair<int, string> a, pair<int, string> b) {
+    if(a.first < b.first) {
+        return true;
+    } else if(a.first == b.first){
+        return false;
+    }
+    return false;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); //cin 속도 향상 위해
+    int T;
+    cin >> T;
+
+    vector<pair<int,string>> v(T);
+
+    for(int i = 0; i < T; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+    stable_sort(v.begin(), v.end(), cmp);
+    // 나이가 같은경우 sort는 순서를 보장하지않는 반면, stable_sort는 순서를 보장해준다.
+
+    for(int i = 0; i < T; i++) {
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+    return 0;
+}
