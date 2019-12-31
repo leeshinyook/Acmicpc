cdabe5d4f4383d210d3d83cd96c62813937038a9
leeshinyook <tlsdbr97@gmail.com>
2020-01-01 00:00:45 +0900

    Add : 2xN 타일링2 #다이나믹프로그래밍

---
+++ 11727.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        long[] dp = new long[T + 1];
+        dp[0] = 1;
+        dp[1] = 3;
+
+        for(int i = 2; i < T + 1; i++) {
+            dp[i] = ((dp[i - 1] + dp[i - 2] * 2))% 10007;
+        }
+        System.out.println(dp[T - 1]);
+    }
+}
