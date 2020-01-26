79bad7ae1c8fe6eae3730d58e22132616eb2c129
leeshinyook <tlsdbr97@gmail.com>
2020-01-26 15:28:05 +0900

    Add : 카드 구매하기2 #다이나믹프로그래밍

---
+++ 16194.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] arr = new int[T + 1];
+        int[] dp = new int[T + 1];
+
+
+        for(int i = 1; i <= T; i++) {
+            arr[i] = sc.nextInt();
+            dp[i] = arr[i];
+        }
+
+        for(int i = 1; i <= T; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+
+        System.out.println(dp[T]);
+
+    }
+}
