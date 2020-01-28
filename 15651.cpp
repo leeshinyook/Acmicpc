951c68d0d04b88be0f0e7045e1160bb901fba23d
leeshinyook <tlsdbr97@gmail.com>
2020-01-29 02:29:29 +0900

    Add : N과 M(3) #백트래킹

---
+++ 15651.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define FASTSTD ios::sync_with_stdio(0)
+#define FASTCIN cin.tie(0)
+using namespace std;
+
+int n, m;
+bool check = false;
+vector<int> permutation;
+bool chosen[1000000];
+void search() {
+    if(permutation.size() == m) {
+        for(int i = 0; i < m; i++) {
+            cout << permutation[i] << " ";
+        }
+        cout << '\n';
+    } else {
+        for(int i = 1; i <= n; i++) {
+//            if(chosen[i]) continue;
+            chosen[i] = true;
+            permutation.push_back(i);
+            search();
+            chosen[i] = false;
+            permutation.pop_back();
+        }
+    }
+}
+int main() {
+    FASTSTD;
+    FASTCIN;
+    cin >> n >> m;
+    search();
+    return 0;
+}
