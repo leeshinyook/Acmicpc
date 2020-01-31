ba51e748e8ebc2e61a4cd1cf5ed6796d9874ce78
leeshinyook <tlsdbr97@gmail.com>
2020-02-01 02:54:25 +0900

    Add : 암호 만들기 #조합

--- 1759.cpp
+++ 1759.cpp
@@ -2,70 +2,65 @@
 #include <vector>
 #include <algorithm>
 
-#define FASTSTD ios::sync_with_stdio(0)
-#define FASTCIN cin.tie(0)
 using namespace std;
-int N, M;
-vector<string> v;
-vector<string> vp;
-vector<string> ans;
-bool chosen[100];
-bool asending = true;
-string vowel = "aeiou";
-int vowel_count = 0;
-int con_count = 0;
-void init() {
-    vowel_count = 0;
-    con_count = 0;
-    asending = true;
-}
-void search() {
-    if(vp.size() == N) {
-        for(int i = 0; i < N - 1; i++) {
-            if(vp[i].compare(vp[i + 1]) > 0) {
-                asending = false;
+int L, C;
+char A[16];
+vector<int> ind;
+int aeiou[16];
+
+bool check() {
+    int cnt = 0; // 모음 개수
+    int cnt2 = 0; // 자음 개수
+    for (int i = 0; i < ind.size(); i++) {
+        if (ind[i] == 1) {
+            if (aeiou[i] == 0) {
+                cnt2++;
+            }
+            else {
+                cnt++;
             }
         }
-        for(int i = 0; i < N; i++) {
-            if (vowel.find(vp[i]) != string::npos) {
-                vowel_count++;
-            } else {
-                con_count++;
-            }
-        }
-        if(asending && vowel_count > 0 && con_count > 1) {
-            string an = "";
-            for(int i = 0; i < N; i++) {
-                an += vp[i];
-            }
-            ans.push_back(an);
-        }
-        init();
-    } else {
-        for(int i = 0; i < M; i++) {
-            if(chosen[i]) continue;
-            chosen[i] = true;
-            vp.push_back(v[i]);
-            search();
-            chosen[i] = false;
-            vp.pop_back();
-        }
+    }
+    if (cnt >= 1 && cnt2 >= 2) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
 
 int main() {
-    FASTSTD;
-    FASTCIN;
-    cin >> N >> M;
-    for(int i = 0; i < M; i++) {
-        string str;
-        cin >> str;
-        v.push_back(str);
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> L >> C;
+    for (int i = 0; i < C; i++) {
+        cin >> A[i];
     }
-    search();
-    sort(ans.begin(), ans.end());
-    for(int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << '\n';
+    sort(A, A + C);
+    for (int i = 0; i < C; i++) {
+        if (A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u') {
+            aeiou[i] = 1;
+        }
     }
-    return 0;
+    for (int i = 0; i < L; i++) {
+        ind.push_back(1);
+    }
+    for (int i = 0; i < C - L; i++) {
+        ind.push_back(0);
+    }
+    //sort(ind.begin(), ind.end());
+    do {
+        if (check()) {
+            for (int i = 0; i < ind.size(); i++) {
+                if (ind[i] == 1) {
+                    cout << A[i];
+                }
+            }
+            cout << endl;
+        }
+    } while (prev_permutation(ind.begin(), ind.end()));
+
 }
