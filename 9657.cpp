33a305d6e0fabbbd819ee0f6af6c1010389d6341
leeshinyook <tlsdbr97@gmail.com>
2020-02-02 01:12:41 +0900

    Add : 돌 게임3 #다이나믹프로그래밍

---
+++ 9657.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<bool> v(1002);
+vector<int> idx = {1, 3, 4};
+int T;
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> T;
+
+    v[1] = true;
+    v[2] = false;
+    v[3] = true;
+    v[4] = true;
+
+    for(int i = 5; i <= 1001; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(!v[i - idx[j]]) {
+                v[i] = true;
+                break;
+            }
+            v[i] = false;
+        }
+    }
+    if(v[T]) {
+        cout << "SK";
+    } else {
+        cout << "CY";
+    }
+
+}
