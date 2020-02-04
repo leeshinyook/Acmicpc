591044f26c0648120e7415bca4f309d0531b29b6
leeshinyook <tlsdbr97@gmail.com>
2020-02-04 22:29:25 +0900

    Add : 연결 요소의 개수 #DFS, BFS

---
+++ main.cpp
@@ -0,0 +1,37 @@
+//
+// Created by 이신육 on 2020-02-03.
+//
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1001][1001];
+bool visited[1001];
+int N, M;
+int cnt = 0;
+void DFS(int s) {
+    visited[s] = true;
+    for(int i = 1; i <= N; i++) {
+        if(arr[s][i] == 1 && !visited[i]) {
+            DFS(i);
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for(int i = 1; i <= M; i++) {
+        int x, y;
+        cin >> x >> y;
+        arr[y][x] = arr[x][y] = 1;
+    }
+    for(int i = 1; i <= N; i++) {
+        if(!visited[i]){
+            DFS(i);
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
