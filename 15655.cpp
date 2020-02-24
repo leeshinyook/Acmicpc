61030aa9c44a88f9eb157e40eee5d6a30d39e462
leeshinyook <tlsdbr97@gmail.com>
2020-02-24 16:41:14 +0900

    Add : N과 M(6) #백트래킹

---
+++ 15655.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v(10);
+vector<int> temp(10);
+int N, M;
+bool checker = false;
+int main() {
+    cin >> N >> M;
+
+    for(int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end() - 10 + N);
+    do {
+        checker = false;
+        for(int i = 0; i < M; i++) {
+            if(temp[i] == v[i]) {
+                checker = true;
+            } else {
+                checker = false;
+                break;
+            }
+        }
+        for(int i = 0; i < M - 1; i++) {
+            if(v[i] > v[i + 1]) {
+                checker = true;
+                break;
+            }
+        }
+        if(!checker) {
+            for(int i = 0; i < M; i++) {
+                cout << v[i] << " ";
+                temp[i] = v[i];
+            }
+            cout << '\n';
+        }
+    } while(next_permutation(v.begin(), v.end() - 10 + N));
+}
+
