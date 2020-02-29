cc553ac4de98e97a6ad902bd5f30e56c6cf4ea78
leeshinyook <tlsdbr97@gmail.com>
2020-03-01 02:31:30 +0900

    Add : 가장 큰 정사각형

---
+++ 1915.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<algorithm>
+using namespace std;
+
+int dp[1001][1001] = { 0 };
+int n, m;
+int ans = 0;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    scanf("%d%d", &n, &m);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            scanf("%1d", &dp[i][j]);
+        }
+    }
+    for(int i = 1; i <= n ; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(dp[i][j] != 0) {
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                if(ans < dp[i][j]) ans = dp[i][j];
+            }
+        }
+    }
+    cout << ans * ans;
+    return 0;
+}
