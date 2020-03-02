c852adf45345c9376e4a6acab26338311b16f803
leeshinyook <tlsdbr97@gmail.com>
2020-03-03 02:14:03 +0900

    Add : 점프 #다이나믹프로그래밍

--- 1890.cpp
+++ 1890.cpp
@@ -8,26 +8,10 @@
 using namespace std;
 
 int arr[101][101];
-int dp[101][101];
+long long dp[101][101];
 
-int N, M, T;
-int cnt = 0;
-void search(int x, int y) {
-    if(x < 1 || x > T || y < 1 || y > T) { // 장외
-        return;
-    }
-    cout << x << " " << y << '\n';
+int T;
 
-    if(x == T && y == T) {
-        cnt++;
-        return;
-    }
-    int num = arr[x][y];
-    // 오른쪽
-    search(x, y + num);
-    // 왼쪽
-    search(x + num, y);
-}
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -38,6 +22,20 @@ int main(){
             cin >> arr[i][j];
         }
     }
-    search(1, 1);
-    cout << cnt;
+    dp[1][1] = 1; // 경로의
+    for(int i = 1; i <= T; i++) {
+        for(int j = 1; j <= T; j++) {
+            if(i == T && j == T) continue;
+            if(!dp[i][j]) continue;
+            int num = arr[i][j];
+            if((j + num) <= T) {
+                dp[i][j + num] = dp[i][j + num] + dp[i][j];
+
+            }
+            if((i + num) <= T) {
+                dp[i + num][j] = dp[i + num][j] + dp[i][j];
+            }
+        }
+    }
+    cout << dp[T][T];
 }
