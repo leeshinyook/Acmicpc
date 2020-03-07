7f7ca856afb93efd7d1842a31321630953e43a95
leeshinyook <tlsdbr97@gmail.com>
2020-03-08 01:52:02 +0900

    Add : 숨바꼭질3 #BFS

---
+++ 13549.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+int N, K;
+int cnt = 0;
+int arr[100001];
+int visited[100001];
+int M = 98765432, ans = 0;
+queue<pair<int, int>> q;
+
+void BFS(int s) {
+    q.emplace(s, 0);
+    while(!q.empty()) {
+        int x = q.front().first;
+        int dist = q.front().second;
+        q.pop();
+        visited[x] = true;
+        if(x > 100000) continue;
+        if(x == K) {
+            cnt++;
+            if(dist <= M) {
+                M = dist;
+                ans++;
+            }
+        }
+        if(0 <= x - 1 && !visited[x - 1]) {
+            q.emplace(x - 1, dist + 1);
+        }
+        if(x + 1<= 100000 && !visited[x + 1]) {
+            q.emplace(x + 1, dist + 1);
+        }
+        if(2 * x <= 100000 && !visited[2 * x]) {
+            q.emplace(2 * x, dist);
+        }
+
+
+    }
+
+}
+int main() {
+    cin >> N >> K;
+    BFS(N);
+    cout << M;
+}
