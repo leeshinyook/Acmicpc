21ebd54ef93c33d736f2a96dd53d07795911cda0
leeshinyook <tlsdbr97@gmail.com>
2020-03-09 16:37:03 +0900

    Add : 보물섬 #BFS

---
+++ 2589.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int arr[51][51];
+int dist[51][51];
+bool visited[51][51];
+int dx[4] = {0, 1, -1, 0};
+int dy[4] = {1, 0, 0, -1};
+
+int N, M;
+int MAX = 0;
+int MIN = 0;
+vector<pair<int, int>> shark;
+queue<pair<int, int>> q;
+
+void BFS(int x, int y) {
+    q.push({x, y});
+    visited[x][y] = true;
+    while(!q.empty()) {
+        int mx = q.front().first;
+        int my = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++) {
+            int lx = dx[i] + mx;
+            int ly = dy[i] + my;
+            if(lx < 1 || lx > N || ly < 1 || ly > M) continue;
+            if(visited[lx][ly] || arr[lx][ly]) continue;
+            dist[lx][ly] = dist[mx][my] + 1;
+            visited[lx][ly] = true;
+            q.push({lx, ly});
+        }
+    }
+}
+void init() { // 초기화
+    for(int i = 0; i < 51; i++) {
+        for(int j = 0; j < 51; j++) {
+            visited[i][j] = false;
+            dist[i][j] = 0;
+        }
+    }
+    while(!q.empty()) {
+        q.pop();
+    }
+}
+void findMin() {
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
+            if(dist[i][j] > MAX) {
+                MAX = dist[i][j];
+            }
+        }
+    }
+}
+int main() {
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
+            char s;
+            cin >> s;
+            if(s == 'L') {
+                arr[i][j] = 0; // 육지
+            } else {
+                arr[i][j] = 1; // 바다
+            }
+        }
+    }
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
+            if(arr[i][j] == 0) {
+                BFS(i, j);
+                findMin();
+                init();
+            }
+        }
+    }
+
+    cout << MAX;
+}
