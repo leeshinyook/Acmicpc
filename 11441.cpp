0ce5d8938bbbeb2fedc8bd38ad092c4148b377e4
leeshinyook <tlsdbr97@gmail.com>
2020-04-24 14:03:22 +0900

    Add : 합 구하기 #다이나믹프로그래밍

---
+++ 11441.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <cstring>
+#include <algorithm>
+#define DES 1000000;
+using namespace std;
+
+int N, M;
+int arr[100001];
+int dp[100001];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    dp[1] = arr[1];
+    for(int i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + arr[i];
+    }
+    cin >> M;
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << dp[b] - dp[a - 1] << '\n';
+    }
+};
